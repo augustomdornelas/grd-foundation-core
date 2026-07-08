bdef86ec13ceb512103fe6ab8f76ea9417cc94fa
augustomdornelas <augustomdornelas@gmail.com>
2026-07-08 18:55:00 -0300

    Update client.server.ts

--- src/integrations/supabase/client.server.ts
+++ src/integrations/supabase/client.server.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fpuwyndpmcgwkuaqbcvm.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwdXd5bmRwbWNnd2t1YXFiY3ZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1MjI1NzEsImV4cCI6MjA5OTA5ODU3MX0.3WXC6A7Xq54tigOMg25gcjoJNj_uzbQFahraeCUPFJE';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabaseServer = createClient(supabaseUrl, supabaseKey);
 export const supabase = supabaseServer;
